18cf438ce963f9ecc70f6d1952a9a65a67cffef2
amyl09 <amylianaamohd@gmail.com>
2023-12-04 08:53:33 +0800

    second commit

---
+++ lab3/appList.Designer.vb
@@ -0,0 +1,135 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class appList
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(appList))
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.ListBoxApps = New System.Windows.Forms.ListBox()
+        Me.btnView = New System.Windows.Forms.Button()
+        Me.btnUpdate = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.linkExit = New System.Windows.Forms.LinkLabel()
+        Me.SuspendLayout()
+        '
+        'btnAdd
+        '
+        Me.btnAdd.BackColor = System.Drawing.Color.AntiqueWhite
+        Me.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnAdd.Font = New System.Drawing.Font("Modern No. 20", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAdd.Location = New System.Drawing.Point(444, 138)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(128, 35)
+        Me.btnAdd.TabIndex = 0
+        Me.btnAdd.Text = "Add"
+        Me.btnAdd.UseVisualStyleBackColor = False
+        '
+        'ListBoxApps
+        '
+        Me.ListBoxApps.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ListBoxApps.ForeColor = System.Drawing.Color.SaddleBrown
+        Me.ListBoxApps.FormattingEnabled = True
+        Me.ListBoxApps.ItemHeight = 19
+        Me.ListBoxApps.Location = New System.Drawing.Point(130, 139)
+        Me.ListBoxApps.Name = "ListBoxApps"
+        Me.ListBoxApps.Size = New System.Drawing.Size(182, 232)
+        Me.ListBoxApps.TabIndex = 1
+        '
+        'btnView
+        '
+        Me.btnView.BackColor = System.Drawing.Color.AntiqueWhite
+        Me.btnView.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnView.Font = New System.Drawing.Font("Modern No. 20", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnView.Location = New System.Drawing.Point(444, 198)
+        Me.btnView.Name = "btnView"
+        Me.btnView.Size = New System.Drawing.Size(128, 35)
+        Me.btnView.TabIndex = 2
+        Me.btnView.Text = "View"
+        Me.btnView.UseVisualStyleBackColor = False
+        '
+        'btnUpdate
+        '
+        Me.btnUpdate.BackColor = System.Drawing.Color.AntiqueWhite
+        Me.btnUpdate.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnUpdate.Font = New System.Drawing.Font("Modern No. 20", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnUpdate.Location = New System.Drawing.Point(444, 263)
+        Me.btnUpdate.Name = "btnUpdate"
+        Me.btnUpdate.Size = New System.Drawing.Size(128, 35)
+        Me.btnUpdate.TabIndex = 3
+        Me.btnUpdate.Text = "Update"
+        Me.btnUpdate.UseVisualStyleBackColor = False
+        '
+        'btnDelete
+        '
+        Me.btnDelete.BackColor = System.Drawing.Color.AntiqueWhite
+        Me.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnDelete.Font = New System.Drawing.Font("Modern No. 20", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDelete.Location = New System.Drawing.Point(444, 325)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(128, 35)
+        Me.btnDelete.TabIndex = 4
+        Me.btnDelete.Text = "Delete"
+        Me.btnDelete.UseVisualStyleBackColor = False
+        '
+        'linkExit
+        '
+        Me.linkExit.AutoSize = True
+        Me.linkExit.BackColor = System.Drawing.Color.Transparent
+        Me.linkExit.Font = New System.Drawing.Font("MS Gothic", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.linkExit.ForeColor = System.Drawing.Color.Transparent
+        Me.linkExit.LinkColor = System.Drawing.Color.Red
+        Me.linkExit.Location = New System.Drawing.Point(533, 21)
+        Me.linkExit.Name = "linkExit"
+        Me.linkExit.Size = New System.Drawing.Size(39, 13)
+        Me.linkExit.TabIndex = 6
+        Me.linkExit.TabStop = True
+        Me.linkExit.Text = "Exit"
+        '
+        'appList
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.lab3.My.Resources.Resources.Product_Trend
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(636, 428)
+        Me.Controls.Add(Me.linkExit)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.btnUpdate)
+        Me.Controls.Add(Me.btnView)
+        Me.Controls.Add(Me.ListBoxApps)
+        Me.Controls.Add(Me.btnAdd)
+        Me.DoubleBuffered = True
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "appList"
+        Me.Text = "appList"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents ListBoxApps As ListBox
+    Friend WithEvents btnView As Button
+    Friend WithEvents btnUpdate As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents linkExit As LinkLabel
+End Class
---
+++ lab3/appList.vb
@@ -0,0 +1,122 @@
+﻿Public Class appList
+    'set the maximum capacity for the array
+    Private maxCapacity As Integer = 4
+    Private apps(maxCapacity - 1) As String
+    Private appCount As Integer = 0
+
+    'display messagebox info so that user know what to do first
+    Private Sub appList_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MessageBox.Show("Please add the apps first", "Important Message", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+
+    'clear the listboxapps 
+    Private Sub clearList()
+        ListBoxApps.Items.Clear()
+    End Sub
+    Private Sub DisplayApps()
+        'display the current apps in a listApp 
+        ListBoxApps.Items.Clear()
+
+        For i As Integer = 0 To appCount - 1
+            ListBoxApps.Items.Add(apps(i))
+        Next
+    End Sub
+
+    Private Sub AddApp(appName As String)
+        'check if there is space in the array
+        If appCount < maxCapacity Then
+            'add the app to the array
+            apps(appCount) = appName
+            appCount += 1
+            MessageBox.Show($"{appName} has been added to the list.", "Add Information", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Else
+            MessageBox.Show("Array is full! Cannot add more apps.", "Add Information", MessageBoxButtons.OK, MessageBoxIcon.Stop)
+        End If
+    End Sub
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        'get the app name from the user
+        Dim appName As String = InputBox("Enter the name of the app: ")
+
+        'check if the user entered a name
+        If Not String.IsNullOrEmpty(appName) Then
+            'add the app to the array
+            addApp(appName)
+
+            'display the updated list of apps
+            DisplayApps()
+            clearList()
+        Else
+            MessageBox.Show("Please enter a valid app name.", "Add Information", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+        End If
+    End Sub
+
+    Private Sub btnView_Click(sender As Object, e As EventArgs) Handles btnView.Click
+        'display the list of all installed apps
+        DisplayApps()
+    End Sub
+
+    Private Sub UpdateApp(index As Integer, newName As String)
+        'update the app at the specified index with the new name
+        apps(index) = newName
+        MessageBox.Show($"The app is successfully updated! New app name: {newName}", "Update Information", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+    Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
+        'allow the user to select an app to update
+        Dim selectedAppIndex As Integer = ListBoxApps.SelectedIndex
+
+        If selectedAppIndex <> -1 Then
+            'get the new name for the app
+            Dim newAppName As String = InputBox($"Enter the new name for the app: ")
+
+            'check if the user entered a new name
+            If Not String.IsNullOrEmpty(newAppName) Then
+                'update the selected app with the new name
+                UpdateApp(selectedAppIndex, newAppName)
+
+                'display the updated list of apps
+                DisplayApps()
+            Else
+                MessageBox.Show("Please enter a valid new app name.", "Update Information", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+            End If
+        Else
+            MessageBox.Show("Please select an app to updated.", "Update Information", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+    End Sub
+
+    Private Sub DeleteApp(index As Integer)
+        'remove the app at the specified index
+        Dim removedAppName As String = apps(index)
+        Dim confirmsg = MessageBox.Show($"Are you sure to deleted {removedAppName}?", "Delete Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+
+        If confirmsg = DialogResult.Yes Then
+            'shift the remaining elements to fill the gap
+            For i As Integer = index To appCount - 2
+                apps(i) = apps(i + 1)
+            Next
+
+            'decrement the app count
+            appCount -= 1
+            MessageBox.Show($"{removedAppName} app has been deleted from the list.")
+        End If
+
+    End Sub
+
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        'allow the user to select an app to remove
+        Dim selectedAppIndex As Integer = ListBoxApps.SelectedIndex
+
+        If selectedAppIndex <> -1 Then
+            'remove the selected app
+            DeleteApp(selectedAppIndex)
+
+            'display the updated list of apps
+            DisplayApps()
+        Else
+            MessageBox.Show("Please select an app to remove.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+        End If
+    End Sub
+
+    Private Sub linkExit_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles linkExit.LinkClicked
+        Application.Exit()
+    End Sub
+End Class
